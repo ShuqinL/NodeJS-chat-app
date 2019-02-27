4798f3a9bfc563c22fc483fa1e846a47d8d1aa2a
L <reneelsq@hotmail.com>
2019-02-27 16:26:56 +0100

    files uploaded

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongo = require("mongodb").MongoClient;
+const client = require("socket.io").listen(4000).sockets;
+
+//connect to mongo
+//let url ="mongodb://mymongouser:hellomongouser123@rnc-shard-00-00-6vzo0.mongodb.net:27017,rnc-shard-00-01-6vzo0.mongodb.net:27017,rnc-shard-00-02-6vzo0.mongodb.net:27017/chat-app?ssl=true&replicaSet=RNC-shard-0&authSource=admin&retryWrites=true";
+let dbUrl = "mongodb+srv://mymongouser:hellomongouser123@rnc-6vzo0.mongodb.net/chat-app?retryWrites=true";
+const url = process.env.MONGODB_URI || dbUrl;
+mongo.connect(url, {useNewUrlParser: true},function(err,db){
+    if(err){
+        throw err;
+    }
+    console.log("MongoDB connected...");
+
+    //connect to Socket.io
+    client.on("connection", function(socket){
+        let chat = db.db("chat-app").collection("chats");
+        
+        //create function to send status
+        sendStatus = function(s){
+            socket.emit("status",s); 
+        }
+
+        //get chat from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err,res){
+            if(err){
+                throw err;
+            }
+            //emit the message
+            socket.emit("output", res);
+        });
+
+        //handle input events
+        socket.on("input", function(data){
+            let name = data.name;
+            let message = data.message;
+
+            //check for name and message
+            if(name =="" || message == ""){
+                //send error status
+                sendStatus("Please enter a name and message");
+            } else {
+                //insert message
+                chat.insert({name:name, message: message}, function(){
+                    client.emit("output", [data]);
+                    //send status object
+                    sendStatus({
+                        message:"Message sent",
+                        clear: true
+                    });
+                });
+            }
+        });
+
+        //handle clear
+        socket.on("clear", function(data){
+            //remove all chats from collection
+            chat.remove({}, function(){
+                //emit cleared
+                socket.emit("cleared");
+            });
+        });
+    });
+});
